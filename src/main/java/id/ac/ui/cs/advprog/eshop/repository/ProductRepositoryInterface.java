0eb8e08b0a3c9bff6cc444a55a216f8d5d9dc7d8
Revaldyhfz <revaldyhafiz16@gmail.com>
2024-02-23 18:52:55 +0700

    Implementing some unimplemented principle

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryInterface.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+
+import java.util.Iterator;
+
+public interface ProductRepositoryInterface {
+    Product create(Product product);
+    Iterator<Product> findAll();
+    Product findById(String id);
+    void update(Product product);
+    void delete(Product product);
+}
